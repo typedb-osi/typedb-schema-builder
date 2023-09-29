2768042f9cc563a5646b6cf31d6b631b8a51682b
Abhishek Srivastava <abhishek232004@gmail.com>
2023-09-29 16:13:32 +0530

    Update Exceptions.py

--- typedbSchemaBuilder/Exceptions.py
+++ typedbSchemaBuilder/Exceptions.py
@@ -8,9 +8,9 @@ import copy
 
 
 class SchemaChecker:
-    def __init__(self, schema: str, query_log: deque, types_: dict) -> None:
+    def __init__(self, schema: str, constraints_log: deque, types_: dict) -> None:
         self.schema = schema
-        self.query_log = query_log
+        self.constraints_log = constraints_log
         self.types = types_
 
     def test(self) -> None:
@@ -21,8 +21,8 @@ class SchemaChecker:
         self.key_unique_ownership_check()
         # Check for defination availability
 
-    def grammar_check(self, query: str) -> bool:
-        lexer = TypeQLLexer(InputStream(query))
+    def grammar_check(self, constraints: str) -> bool:
+        lexer = TypeQLLexer(InputStream(constraints))
         lexer.removeErrorListeners()  # Remove default error listeners
         lexer.addErrorListener(MyErrorListener())
         stream = CommonTokenStream(lexer)
@@ -35,40 +35,40 @@ class SchemaChecker:
             raise Exception(e)
 
     def check_regex(self) -> None:
-        query_log_twin = copy.deepcopy(self.query_log)
-        n = len(query_log_twin)
+        constraints_log_twin = copy.deepcopy(self.constraints_log)
+        n = len(constraints_log_twin)
         for i in range(0, n):
-            query = query_log_twin[0]
-            query_log_twin.popleft()
-            if query[0] == "regex":
-                expression = r"" + query[2]
+            constraints = constraints_log_twin[0]
+            constraints_log_twin.popleft()
+            if constraints[0] == "regex":
+                expression = r"" + constraints[2]
                 re.compile(expression)
 
     # Does not work properly (flawed logic), needs to be re-written after finalising the requirements
     def abstract_match_check(self) -> None:
-        query_log_twin = copy.deepcopy(self.query_log)
-        n = len(query_log_twin)
+        constraints_log_twin = copy.deepcopy(self.constraints_log)
+        n = len(constraints_log_twin)
         for i in range(0, n):
-            query = query_log_twin[0]
-            query_log_twin.popleft()
+            constraints = constraints_log_twin[0]
+            constraints_log_twin.popleft()
             abstract_count = [0, 0]
-            for j in range(0, len(query)):
-                if query[j] in self.types.keys():
-                    if len(self.types[query[j]].super_types) == 0:
+            for j in range(0, len(constraints)):
+                if constraints[j] in self.types.keys():
+                    if len(self.types[constraints[j]].super_types) == 0:
                         continue
-                    abstract_count[self.types[query[j]].abstract] += 1
+                    abstract_count[self.types[constraints[j]].abstract] += 1
             if abstract_count[0] and abstract_count[1]:
-                raise Exception("Error: Mixed types\nqid:" + str(query[-1]))
+                raise Exception("Error: Mixed types\nqid:" + str(constraints[-1]))
 
     def super_type_check(self) -> None:
-        query_log_twin = copy.deepcopy(self.query_log)
-        n = len(query_log_twin)
+        constraints_log_twin = copy.deepcopy(self.constraints_log)
+        n = len(constraints_log_twin)
         for i in range(0, n):
-            query = query_log_twin[0]
-            query_log_twin.popleft()
-            if query[0] == "sub":
-                type = query[2]
-                subtype = query[1]
+            constraints = constraints_log_twin[0]
+            constraints_log_twin.popleft()
+            if constraints[0] == "sub":
+                type = constraints[2]
+                subtype = constraints[1]
                 if type not in self.types.keys():
                     raise Exception(
                         "Error defining subtype:"
@@ -76,7 +76,7 @@ class SchemaChecker:
                         + "\nThe type:"
                         + type
                         + "does not exist\nqid:"
-                        + str(query[-1]),
+                        + str(constraints[-1]),
                     )
                 if subtype in self.types.keys():
                     if len(self.types[subtype].super_types) > 1:
@@ -84,7 +84,7 @@ class SchemaChecker:
                             "Error defining subtype:"
                             + subtype
                             + "\nThe subtype is already defined\nqid:"
-                            + str(query[-1])
+                            + str(constraints[-1])
                         )
                     elif (
                         len(self.types[subtype].super_types)
@@ -94,18 +94,18 @@ class SchemaChecker:
                             "Error defining subtype:"
                             + subtype
                             + "\nThe subtype is already defined\nqid:"
-                            + str(query[-1])
+                            + str(constraints[-1])
                         )
 
     def key_unique_ownership_check(self) -> None:
-        query_log_twin = copy.deepcopy(self.query_log)
-        n = len(query_log_twin)
+        constraints_log_twin = copy.deepcopy(self.constraints_log)
+        n = len(constraints_log_twin)
         for i in range(0, n):
-            query = query_log_twin[0]
-            query_log_twin.popleft()
-            if query[0] == "key" or query[0] == "unique":
-                type_ = query[1]
-                owns = query[2]
+            constraints = constraints_log_twin[0]
+            constraints_log_twin.popleft()
+            if constraints[0] == "key" or constraints[0] == "unique":
+                type_ = constraints[1]
+                owns = constraints[2]
                 if owns not in self.types[type_].attributes:
                     message = (
                         "Error: Type = "
@@ -113,138 +113,138 @@ class SchemaChecker:
                         + " does not own:"
                         + owns
                         + "\nqid:"
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
 
     def predefined_type_check(self) -> None:
-        query_log_twin = copy.deepcopy(self.query_log)
-        n = len(query_log_twin)
+        constraints_log_twin = copy.deepcopy(self.constraints_log)
+        n = len(constraints_log_twin)
         for i in range(0, n):
-            query = query_log_twin[0]
-            query_log_twin.popleft()
-            if query[0] in ["abstract", "value", "regex"]:
-                if query[1] not in self.types.keys():
+            constraints = constraints_log_twin[0]
+            constraints_log_twin.popleft()
+            if constraints[0] in ["abstract", "value", "regex"]:
+                if constraints[1] not in self.types.keys():
                     message = (
                         "Error: type = "
-                        + query[1]
+                        + constraints[1]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
 
-            elif query[0] in ["relates", "relates_as"]:
-                if query[1] not in self.types.keys():
+            elif constraints[0] in ["relates", "relates_as"]:
+                if constraints[1] not in self.types.keys():
                     message = (
                         "Error: type = "
-                        + query[1]
+                        + constraints[1]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-                elif "relation" != self.types[query[1]].root_type:
+                elif "relation" != self.types[constraints[1]].root_type:
                     message = (
                         "Error:"
-                        + query[1]
+                        + constraints[1]
                         + " is not an relation type"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-            elif query[0] in ["owns", "owns_as", "key", "unique"]:
-                if query[1] not in self.types.keys():
+            elif constraints[0] in ["owns", "owns_as", "key", "unique"]:
+                if constraints[1] not in self.types.keys():
                     message = (
                         "Error: type = "
-                        + query[1]
+                        + constraints[1]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-                elif "entity" != self.types[query[1]].root_type:
+                elif "entity" != self.types[constraints[1]].root_type:
                     message = (
                         "Error:"
-                        + query[1]
+                        + constraints[1]
                         + " is not an entity type"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
 
-                if query[-2] not in self.types.keys():
+                if constraints[-2] not in self.types.keys():
                     message = (
                         "Error:"
-                        + query[-2]
+                        + constraints[-2]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-                elif "attribute" != self.types[query[-2]].root_type:
+                elif "attribute" != self.types[constraints[-2]].root_type:
                     message = (
                         "Error:"
-                        + query[-2]
+                        + constraints[-2]
                         + " is not an attribute type"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-            elif query[0] in ["sub"]:
-                if query[2] not in self.types.keys():
+            elif constraints[0] in ["sub"]:
+                if constraints[2] not in self.types.keys():
                     message = (
                         "Error: type = "
-                        + query[2]
+                        + constraints[2]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-            elif query[0] in ["plays", "plays_as"]:
-                if query[1] not in self.types.keys():
+            elif constraints[0] in ["plays", "plays_as"]:
+                if constraints[1] not in self.types.keys():
                     message = (
                         "Error: type = "
-                        + query[1]
+                        + constraints[1]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-                if "entity" != self.types[query[1]].root_type:
+                if "entity" != self.types[constraints[1]].root_type:
                     message = (
                         "Error: not entity, type = "
-                        + query[1]
+                        + constraints[1]
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
 
-                if query[2] not in self.types.keys():
+                if constraints[2] not in self.types.keys():
                     message = (
                         "Error: relationship type = "
-                        + query[2]
+                        + constraints[2]
                         + " is not defined"
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
-                if "relation" != self.types[query[2]].root_type:
+                if "relation" != self.types[constraints[2]].root_type:
                     message = (
                         "Error: not relationship, type = "
-                        + query[2]
+                        + constraints[2]
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
 
-                if query[-2] not in self.types[query[2]].relation_roles:
+                if constraints[-2] not in self.types[constraints[2]].relation_roles:
                     message = (
                         "Error: relationship type = "
-                        + query[2]
+                        + constraints[2]
                         + " has no role = "
-                        + query[-2]
+                        + constraints[-2]
                         + "\nqid: "
-                        + str(query[-1])
+                        + str(constraints[-1])
                     )
                     raise Exception(message)
